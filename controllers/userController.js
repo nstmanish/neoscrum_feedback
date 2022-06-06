37d910ded88b5153ae707972c5b02305248f50dd
nstmanish <manish.chourasiya@neosoftmail.com>
2022-06-06 17:58:31 +0530

    clean config

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,16 +20,12 @@ const User = require('../models/userModel');
  * This function Register new Employee | Admin can only add Employee
  */
 exports.register = async ( req, res ) => {
-
     try {
-
         const { name, email, profile } = req.body;
         // Validation
         const generatedPassword = Password.password;
-        
         // Encrypt Password
         encryptedPassword = await bcrypt.hash(generatedPassword, 10);
-
         // Add User
         let user = await User.create({
             name,
@@ -38,40 +34,28 @@ exports.register = async ( req, res ) => {
             password : encryptedPassword, 
             token: null,   
         });
-
         // Send Mail
         await mailer.sendPassword(user, generatedPassword);
-
         // Send Response
         res.status(StatusCodes.OK).json({user});
-
     }catch(err){
         console.log(err);
         res.status(StatusCodes.BAD_REQUEST).send(err);
-
     }
-
 }
 
 // Login
 exports.login = async ( req, res ) => {
-
     try {
-
         const { email, password } = req.body;
-
         // Get The user
         const user = await User.findOne({ email });
-
         // Check user exist
         if (!user) { return res.status(StatusCodes.NOT_FOUND).json(user) }
-
         // Check the password
         if (!(await bcrypt.compare(password, user.password))) { return res.status(StatusCodes.FORBIDDEN).json("dd") }
-
         // Send JWT
         if (user && (await bcrypt.compare(password, user.password))) {
-          
             const token = await jwt.sign(
                 { user_id: user._id, email, is_admin : user.isAdmin  },
                 process.env.TOKEN_KEY,
@@ -79,22 +63,16 @@ exports.login = async ( req, res ) => {
                     expiresIn: "5h",
                 }
             );
-
             user.token = token;
-
             return res.status(StatusCodes.OK).json(user);
         }
-
         return res.status(StatusCodes.BAD_REQUEST).json({user});
-
-    }catch(err){
-
-        res.status(StatusCodes.BAD_REQUEST).send(err);
-
-    }
+    }catch(err){ res.status(StatusCodes.BAD_REQUEST).send(err) }
 }
 
 // logout
 exports.logout = async ( req, res ) => {
-    
+    try {
+        res.status(StatusCodes.OK)
+    }catch(err){  res.status(StatusCodes.BAD_REQUEST).send(err) }
 }

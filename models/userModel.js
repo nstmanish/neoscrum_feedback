3d6f2932d97cfb95d08d44a2b56a53a9b8c4a73d
nstmanish <manish.chourasiya@neosoftmail.com>
2022-06-06 17:44:12 +0530

    added admin privilage

--- models/userModel.js
+++ models/userModel.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
     email     : { type: String, unique: true, required: true },
     profile   : { type: String, default: "https://picsum.photos/200" },
     password  : { type: String, required: true },
+    isAdmin   : { type: Boolean, default: false },
     token     : { type: String, default: null },
 });
 
